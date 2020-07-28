ede26895a2e6582654b4713bec88ac7a410326a0
Ubuntu <ubuntu@ip-172-31-46-59.ap-northeast-1.compute.internal>
2020-07-28 22:55:12 +0000

    Add user-sarch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   include SessionsHelper
-
+  
   private
 
+    def microposts_search_params
+      params.require(:q).permit(:content_cont)
+    end
+    
     # ユーザーのログインを確認する
     def logged_in_user
       unless logged_in?
